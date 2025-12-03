5f37c327687e7926abf0df9c7c6b4fa5f06cb026
Eias <aboomar5464@gmail.com>
2025-12-03 05:18:17 -0800

    Module 01 Assignment - Express + React

--- src/employees.jsx
+++ src/employees.jsx
@@ -4,7 +4,7 @@ let holly = 'Holly Unlikely'
 let amol = 'Amol Shookup'
 let eias = 'Eias Alhomsi'
 const element = (
-    <ul style={{'color':'blue', 'fontSize': '24px'}}>
+    <ul style={{'color':'DarkGreen', 'fontSize': '24px'}}>
         <li>{sally}</li>
         <li>{mark}</li>
         <li>{holly}</li>
